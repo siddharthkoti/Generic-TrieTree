272bf51cac3158f2e5975f548d60aee9c3191ee6
Dhamodhran <svella9@gmail.com>
2017-06-26 18:44:46 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -9,16 +9,17 @@ int main()
 	t.insert("zebra",5);
 	t.insert("boomerang",6);
 	t.insert("apple",1);
-	t.insert("saithan",8);
+	t.insert("sample",8);
 
 	TrieTree<string> t1;
 	t1.insert("dameit","zebra");
-	t1.insert("edinture","comeon");
+	t1.insert("batman","comeon");
 
-	cout<<t1["edinture"]<<"\n";
-	t1["edinture"] = "gotit";
-	cout<<t1["edinture"]<<"\n";
+	cout<<t1["batman"]<<"\n";
+	t1["batman"] = "gotit";
+	cout<<t1["batman"]<<"\n";
 	t1["dhoni"] = "msd";
 	cout<<t1["dhoni"]<<"\n";
+	
 	return 0;
 }
